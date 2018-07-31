f87529a9da53398e9af5facc80248e02b7a761f8
Ray Dise <raydise@gediprime.attlocal.net>
2018-07-30 21:17:58 -0500

    Upgraded to font-awesome 5 and updated components to use the new syntax

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, FontAwesomeModule
   ],
   declarations: [LayoutComponent, HeaderComponent, FooterComponent],
   exports: [LayoutComponent]
