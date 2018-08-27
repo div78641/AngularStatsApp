0e6268b11ef38a99eb2e548d825864021c69d52f
Ray Dise <raydise@gmail.com>
2018-08-27 13:56:07 -0500

    Added MLB Home component

--- src/app/mlb/mlb-routing.module.ts
+++ src/app/mlb/mlb-routing.module.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { StandingsComponent } from './standings/standings.component';
 import { RunsperhitComponent } from './runsperhit/runsperhit.component';
 import { GamelogComponent } from './gamelog/gamelog.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: 'mlb', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'mlb', component: HomeComponent },
       { path: 'mlb/standings', component: StandingsComponent },
       { path: 'mlb/gamelog', component: GamelogComponent },
       { path: 'mlb/runsperhit', component: RunsperhitComponent }
--- src/app/mlb/mlb.module.ts
+++ src/app/mlb/mlb.module.ts
@@ -8,12 +8,13 @@ import { StandingsComponent } from './standings/standings.component';
 import { RunsperhitComponent } from './runsperhit/runsperhit.component';
 import { GamelogComponent } from './gamelog/gamelog.component';
 import { MlbRoutingModule } from './/mlb-routing.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   imports: [
     CommonModule, BrowserAnimationsModule, MatTableModule, MatSortModule, MatProgressSpinnerModule, FontAwesomeModule, MlbRoutingModule
   ],
-  declarations: [StandingsComponent, RunsperhitComponent, GamelogComponent],
+  declarations: [StandingsComponent, RunsperhitComponent, GamelogComponent, HomeComponent],
   exports: [StandingsComponent, RunsperhitComponent, GamelogComponent]
 })
 export class MlbModule { }
